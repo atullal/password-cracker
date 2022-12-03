55424104d0a0b81bfaf307b7be067fa700baff78
Atul Lal <atul.lal123@gmail.com>
2022-12-03 18:42:39 -0500

    Adds missing s

--- password-cracker/src/Api.js
+++ password-cracker/src/Api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const base = "127.0.0.1:5000";
-export const api = `http://${base}/`;
+export const api = `https://${base}/`;
 export const wsApi = `ws://${base}/password`;
 
 export const getInfo = () => {
